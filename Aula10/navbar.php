23e129bdad1153b8eb7cba5763178d1ecab490b4
LeonardoYur <162757807+LeonardoYur@users.noreply.github.com>
2024-10-29 19:38:27 -0300

    Add files via upload

--- Aula10/navbar.php
+++ Aula10/navbar.php
@@ -1,30 +1,32 @@
-
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#" style="margin:10px">Notas</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav"
+    aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="formularioTurmas.php">Cadastrar turmas</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="formularioAluno.php">Cadastrar alunos</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="formularioNota.php">Cadastrar notas</a>
-      </li>
       <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Mostrar
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li><a class="dropdown-item" href="mostrarAluno.php">Alunos</a></li>
-            <li><a class="dropdown-item" href="mostrarTurmas.php">Turmas</a></li>
-            <li><a class="dropdown-item" href="mostrarNotas.php">Notas</a></li>
-          </ul>
-        </li>
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown"
+          aria-expanded="false">
+          Cadastrar
+        </a>
+        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <li><a class="dropdown-item" href="cadastrarTurma.php">Turmas</a></li>
+          <li><a class="dropdown-item" href="cadastrarAluno.php">Alunos</a></li>
+          <li><a class="dropdown-item" href="cadastrarNota.php">Notas</a></li>
+        </ul>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown"
+          aria-expanded="false">
+          Mostrar
+        </a>
+        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <li><a class="dropdown-item" href="mostrarTurmas.php">Turmas</a></li>
+          <li><a class="dropdown-item" href="mostrarAluno.php">Alunos</a></li>
+          <li><a class="dropdown-item" href="mostrarNotas.php">Notas</a></li>
+        </ul>
+      </li>
     </ul>
   </div>
 </nav>
